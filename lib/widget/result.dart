bf3a2a3f0a1ad47f82cf1885b6f0644d31e36a57
= <=>
2021-03-12 09:13:15 +0700

    Soal 2

--- lib/widget/result.dart
+++ lib/widget/result.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key key, @required double result})
-      : _result = result,
+  const Result(
+      {Key key,
+      @required double reamur,
+      @required double kelvin,
+      @required double fahrenheit})
+      : _reamur = reamur,
+        _kelvin = kelvin,
+        _fahrenheit = fahrenheit,
         super(key: key);
 
-  final double _result;
+  final double _reamur;
+  final double _kelvin;
+  final double _fahrenheit;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
         children: [
-          Text("Hasil Konversi"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Kelvin",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    '$_kelvin',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Fahrenheit",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    '$_fahrenheit',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Reamur",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    '$_reamur',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ],
+              ),
+            ],
+          ),
           Container(
             margin: EdgeInsets.only(top: 20, bottom: 20),
-            child: Text(
-              _result.toStringAsFixed(2),
-              style: TextStyle(fontSize: 36),
-            ),
           ),
         ],
       ),
